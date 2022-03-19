6e48d81c3fe8eccd08e9f5e83e2de693f2777b5d
Marilyn Burnett <burnett.marilyn@ufl.edu>
2022-03-19 13:55:48 -0500

    Code Cleanup

--- form/error.inc.php
+++ form/error.inc.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Error Fields</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Error Fields</title>
 
-        <!-- link to favicon -->
-        <link rel="icon" href="images/favicon.ico">
+      <!-- link to favicon -->
+      <link rel="icon" href="images/favicon.ico">
 
-        <!-- link to sytle.css -->
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
+      <!-- link to sytle.css -->
+      <link rel="stylesheet" type="text/css" href="css/style.css" />
 
-        <!-- Google Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Qwitcher+Grypen:wght@700&display=swap" rel="stylesheet">
+      <!-- Google Fonts -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Qwitcher+Grypen:wght@700&display=swap" rel="stylesheet">
     </head>
 
     <!-- Main Content -->
     <body>
       <div class="container">
         <header>
-            <figure class="navleft">
-            <a href="index.html"><img src="images/sunlogo.svg" alt="ASC Logo" title="Sun Logo" width="140"/></a>
-            </figure>
-            <nav id="navright">
-                <label for="hamburger">&#9776;</label>
-                <input type="checkbox" id="hamburger"/>
-                <div id="hamitems">
-                    <a href="index.html">HOME</a>
-                    <a href="events.html">EVENTS</a>
-                    <a href="about.html">ABOUT</a>
-                    <a href="form.html">CONTACT</a>
-                </div>
-            </nav>
+          <figure class="navleft">
+            <a href="index.html"><img src="images/sunlogo.svg" alt="ASC Logo" title="Sun Emblem" width="140"/></a>
+          </figure>
+          <nav id="navright">
+            <label for="hamburger">&#9776;</label>
+            <input type="checkbox" id="hamburger"/>
+            <div id="hamitems">
+              <a href="index.html">HOME</a>
+              <a href="events.html">EVENTS</a>
+              <a href="about.html">ABOUT</a>
+              <a href="form.html">CONTACT</a>
+            </div>
+          </nav>
 
-            <h1>Arlington Senior Center</h1>
+          <h1>Arlington Senior Center</h1>
         </header>
             
         <main>
